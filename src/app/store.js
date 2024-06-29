062dd72792ede586797d6e67c6e7dd6d4e53dead
arnob-das <contact.arnobdas@gmail.com>
2024-06-29 21:49:18 +0600

    state persist done after refreshing in redux toolkit with redux-persist

--- src/app/store.js
+++ src/app/store.js
@@ -6,15 +6,29 @@ import mealDepositSlice from "../features/meal/mealDepositSlice";
 import utilityBillSclice from "../features/utilityBill/utilityBillSclice";
 import gorceryCostSlice from "../features/groceryCost/gorceryCostSlice";
 
+import storage from "redux-persist/lib/storage";
+import { persistReducer as persistReducerToolkit } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+};
+
+const reducer = combineReducers({
+    auth: authSlice,
+    mess: messSlice,
+    mealCount: mealCountSlice,
+    mealDeposit: mealDepositSlice,
+    utilityBill: utilityBillSclice,
+    groceryCosts: gorceryCostSlice
+})
+
+const persistedReducer = persistReducerToolkit(persistConfig, reducer);
+
 const store = configureStore({
-    reducer: {
-        auth: authSlice,
-        mess: messSlice,
-        mealCount:mealCountSlice,
-        mealDeposit:mealDepositSlice,
-        utilityBill:utilityBillSclice,
-        groceryCosts:gorceryCostSlice
-    }
+    reducer: persistedReducer
 })
 
 export default store;
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,18 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './app/store.js'
 
+import { PersistGate } from "redux-persist/integration/react"
+import { persistStore } from "redux-persist"
+
+let persistor = persistStore(store);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
